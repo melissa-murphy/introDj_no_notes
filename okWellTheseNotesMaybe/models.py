116419770994cc3d71ca901410944e040b865241
melissa-murphy <melissamurphy.ca@gmail.com>
2020-02-10 22:14:02 -0800

    migrations good

--- okWellTheseNotesMaybe/models.py
+++ okWellTheseNotesMaybe/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
-class Note(models.Models):
-    title = models.CharField(_(""), max_length=200)
+
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title=models.CharField(max_length=200)
+    content=models.TextField(blank=True)
