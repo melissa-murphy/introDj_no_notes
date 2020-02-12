a1fff71ebbc3a5417237e41b3f1e7fb03e93c9d0
melissa-murphy <melissamurphy.ca@gmail.com>
2020-02-12 13:39:22 -0800

    working locally w/ dj_database_url

---
+++ Procfile
@@ -0,0 +1,2 @@
+  
+web: gunicorn djNoNotes.wsgi --log-file -
--- djNoNotes/settings.py
+++ djNoNotes/settings.py
@@ -11,15 +11,15 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import psycopg2
 from decouple import config
-import dj_database_url
 import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# DATABASE_URL = config('DATABASE_URL')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -106,6 +106,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 # }
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+DATABASES['default']['NAME'] = os.path.join(BASE_DIR, 'db.sqlite3')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
